ad3bde25631bc341e5031a49aeff42280105a047
Hunteraulo1 <hunteraulo@pm.me>
2024-05-31 09:55:31 +0200

    :bug: filteredList reloaded and tags filter

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,5 +1,6 @@
 import { writable } from 'svelte/store'
 
+import tags from '$lib/assets/tags.json'
 import type { GameType } from './schemas'
 import type { ComboBox, Update } from './types'
 
@@ -23,6 +24,12 @@ export const filter = writable<ComboBox[]>([
     title: 'type',
     open: false,
     values: ['RenPy', 'RPGM', 'Unreal', 'HTLM', 'Flash', 'QSP', 'RenPy/RPGM', 'RenPy/Unity', 'Autre'],
+    selectedValues: [],
+  },
+  {
+    title: 'tags',
+    open: false,
+    values: tags,
     selectedValues: [],
   },
 ])
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,12 +3,12 @@ import type { GameType, UpdateType } from './schemas'
 export interface Update {
   type: UpdateType['type']
   date: Date
-  games: (GameType | number)[]
+  games: (GameType | number | string)[]
 }
 
 export interface ComboBox {
   title: keyof GameType
   open: boolean
-  values: GameType['domain' | 'status' | 'type'][]
+  values: (GameType['domain' | 'status' | 'type'] | string)[]
   selectedValues: string[]
 }
