15a39fa70c73b1b0744b336bd6e61b1dd7ad435e
Hunteraulo1 <hunteraulo@pm.me>
2024-06-19 17:02:22 +0200

    :recycle: remove svelte-query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,47 +2,17 @@
   import Nav from '$lib/components/Nav.svelte'
   import { ScrollArea } from '$lib/components/ui/scroll-area/index'
   import getData from '$lib/utils/getData'
-  import {
-    QueryClient,
-    QueryClientProvider,
-    createWebStoragePersistor,
-    persistQueryClient,
-  } from '@sveltestack/svelte-query'
   import { ModeWatcher } from 'mode-watcher'
   import '../app.postcss'
 
   getData()
-
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        cacheTime: 1000 * 60 * 60 * 6, // 6 hours
-        staleTime: 1000 * 60 * 60 * 6, // 6 hours
-      },
-    },
-  })
-
-  const localStoragePersistor = createWebStoragePersistor({
-    storage: window.localStorage,
-    key: 'f95list-ext',
-    throttleTime: 1000,
-    deserialize: data => JSON.parse(data),
-    serialize: data => JSON.stringify(data),
-  })
-
-  persistQueryClient({
-    queryClient,
-    persistor: localStoragePersistor,
-  })
 </script>
 
 <ModeWatcher />
 
-<QueryClientProvider client={queryClient}>
-  <main class="h-full max-h-full flex flex-col relative">
-    <ScrollArea class="relative h-full min-h-full">
-      <slot />
-    </ScrollArea>
-    <Nav />
-  </main>
-</QueryClientProvider>
+<main class="h-full max-h-full flex flex-col relative">
+  <ScrollArea class="relative h-full min-h-full">
+    <slot />
+  </ScrollArea>
+  <Nav />
+</main>
--- src/routes/traductors/+page.svelte
+++ src/routes/traductors/+page.svelte
@@ -2,68 +2,83 @@
   import { Button } from '$lib/components/ui/button'
   import * as Card from '$lib/components/ui/card'
   import { TraductorsData, type TraductorType } from '$lib/schemas'
-  import { useQuery } from '@sveltestack/svelte-query'
+  import { onMount } from 'svelte'
   import { Reload } from 'svelte-radix'
   import { parse } from 'valibot'
 
-  const queryResult = useQuery<TraductorType[]>({
-    queryFn: async () => {
-      console.log('🚀 ~ queryFn: ~ fetch')
-      try {
-        const response = await fetch(
-          'https://script.google.com/macros/s/AKfycbze5voXSJMnTqnUIgXFgM1gtQ6e13VyAsJK50Evl5Y2HisCRN-eCbbIHXrBaNPhup1J/exec'
-        )
-        const data = await response.json()
+  let queryResult: Promise<TraductorType[]> = Promise.resolve([])
 
-        return data
-      } catch (error) {
-        console.error('Erreur lors de la récupération des données :', error)
-      }
-    },
-    queryKey: 'traductors',
-    select(data: any) {
-      return parse(TraductorsData, data.data)
-    },
-    cacheTime: 1000 * 60 * 60 * 6, // 6 hours
+  onMount(async () => {
+    const date = Date.now()
+
+    const expriredTime = Date.parse(window.localStorage.getItem('f95list_ext_time') ?? '0')
+
+    if (date >= expriredTime) {
+      const queryData = await query()
+
+      window.localStorage.setItem('f95list_ext_data', JSON.stringify(queryData))
+      window.localStorage.setItem('f95list_ext_time', (date + 1000 * 60 * 60 * 24).toString()) // 24 hours
+    }
+
+    queryResult = JSON.parse(window.localStorage.getItem('f95list_ext_data') ?? '[]')
   })
+
+  const query = async (): Promise<TraductorType[]> => {
+    console.log('🚀 ~ queryFn: ~ fetch')
+
+    try {
+      const response = await fetch(
+        'https://script.google.com/macros/s/AKfycbze5voXSJMnTqnUIgXFgM1gtQ6e13VyAsJK50Evl5Y2HisCRN-eCbbIHXrBaNPhup1J/exec'
+      )
+      const data = await response.json()
+
+      return parse(TraductorsData, data?.data)
+    } catch (error) {
+      console.error(error)
+
+      return []
+    }
+  }
 </script>
 
-{#if $queryResult.isSuccess}
-  <div class="flex flex-col gap-4 max-h-full p-2 relative">
-    {#each $queryResult.data as { name, pages, tradCount, readCount }}
-      <Card.Root class="relative">
-        <Card.CardContent class="p-2 min-h-20">
-          <p class="font-bold text-center">{name}</p>
+{#await queryResult then traductors}
+  {#if traductors.length > 0}
+    <div class="flex flex-col gap-4 max-h-full p-2 relative">
+      {#each traductors as { name, pages, tradCount, readCount }}
+        <Card.Root class="relative">
+          <Card.CardContent class="p-2 min-h-20">
+            <p class="font-bold text-center">{name}</p>
 
-          <div class="flex text-sm items-center justify-center">
-            <p class="font-bold text-secondary-foreground/50">Liens:</p>
-            {#each pages as { title, link }}
-              <a href={link} target="_blank">
-                <Button variant="link" class="px-1">
-                  {title}
-                </Button>
-              </a>
-            {:else}
-              <p class="px-1 py-2">Aucun lien</p>
-            {/each}
-          </div>
-          <div class="flex justify-around w-full">
-            <p class="text-sm font-bold text-secondary-foreground/50">
-              Traduction: <span class="text-secondary-foreground">{tradCount}</span>
-            </p>
-            <p class="text-sm font-bold text-secondary-foreground/50">
-              Relecture: <span class="text-secondary-foreground">{readCount}</span>
-            </p>
-          </div>
-        </Card.CardContent>
-      </Card.Root>
-    {/each}
-  </div>
-{:else}
-  <div class="flex justify-center items-center min-h-full">
-    <Button>
-      <Reload class="mr-2 h-4 w-4 animate-spin" />
-      Veuillez patienter
-    </Button>
-  </div>
-{/if}
+            <div class="flex text-sm items-center justify-center">
+              <p class="font-bold text-secondary-foreground/50">Liens:</p>
+              {#each pages as { title, link }}
+                <a href={link} target="_blank">
+                  <Button variant="link" class="px-1">
+                    {title}
+                  </Button>
+                </a>
+              {:else}
+                <p class="px-1 py-2">Aucun lien</p>
+              {/each}
+            </div>
+            <div class="flex justify-around w-full">
+              <p class="text-sm font-bold text-secondary-foreground/50">
+                Traduction: <span class="text-secondary-foreground">{tradCount}</span>
+              </p>
+              <p class="text-sm font-bold text-secondary-foreground/50">
+                Relecture: <span class="text-secondary-foreground">{readCount}</span>
+              </p>
+            </div>
+          </Card.CardContent>
+        </Card.Root>
+      {/each}
+    </div>
+  {:else}
+    <div class="flex justify-center items-center min-h-full">
+      <Button>
+        <Reload class="mr-2 h-4 w-4 animate-spin" />
+        Veuillez patienter
+      </Button>
+    </div>
+  {/if}
+{/await}
