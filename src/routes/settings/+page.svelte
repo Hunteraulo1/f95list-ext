e7fa60f375495bd6c66e5b6822402a9f09fe78d3
Hunteraulo1 <hunteraulo@pm.me>
2024-06-23 11:17:44 +0200

    :rotating_light: lint config changed

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 import { goto } from '$app/navigation'
 import { Button } from '$lib/components/ui/button/index.js'
+import Label from '$lib/components/ui/label/label.svelte'
+import Switch from '$lib/components/ui/switch/switch.svelte'
 import { settings } from '$lib/stores'
 import type { Settings } from '$lib/types'
 import { toggleMode } from 'mode-watcher'
 import DiscordLogo from 'svelte-radix/DiscordLogo.svelte'
 import Moon from 'svelte-radix/Moon.svelte'
 import Sun from 'svelte-radix/Sun.svelte'
-import Label from './../../lib/components/ui/label/label.svelte'
-import Switch from './../../lib/components/ui/switch/switch.svelte'
 
 interface SettingItem {
   title: string
@@ -71,12 +71,20 @@ const handleSettings = (id: keyof Settings) => {
       <div class="flex justify-center items-center gap-2">
         <Label for={id}>{title}</Label>
 
-        {#if id !== 'theme'}
-          <Switch {id} on:click={() => handleSettings(id)} checked={$settings[id]} />
+        {#if id !== "theme"}
+          <Switch
+            {id}
+            on:click={() => handleSettings(id)}
+            checked={$settings[id]}
+          />
         {:else}
           <Button {id} on:click={toggleMode} variant="outline" size="icon">
-            <Sun class="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon class="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <Sun
+              class="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+            />
+            <Moon
+              class="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+            />
             <span class="sr-only">Changer le thème</span>
           </Button>
         {/if}
@@ -87,11 +95,13 @@ const handleSettings = (id: keyof Settings) => {
   <div>
     <h2 class="text-center mb-2 font-bold">En savoir plus</h2>
     <div class="flex justify-center items-center flex-col">
-      <Button variant="outline" on:click={() => goto('/traductors')}>Voir les traducteurs/relecteurs</Button>
+      <Button variant="outline" on:click={() => goto("/traductors")}
+        >Voir les traducteurs/relecteurs</Button
+      >
 
       {#each links as { title, href }}
         <a {href} target="_blank">
-          {#if title !== 'Accèder au Discord'}
+          {#if title !== "Accèder au Discord"}
             <Button variant="link" class="flex gap-1">{title}</Button>
           {:else}
             <Button variant="link" class="flex gap-1">
