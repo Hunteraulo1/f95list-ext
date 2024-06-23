575f0848f4e0fd7a72184d4f588894bafe201353
Hunteraulo1 <hunteraulo@pm.me>
2024-06-23 15:39:12 +0200

    :recycle: adapter svelte

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 // eslint-disable-next-line import/default
 import adapter from 'sveltekit-adapter-chrome-extension'
 
+const buildMode = process.env.BUILD_MODE
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -12,23 +14,13 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(
-      process.env.BUILD_MODE === 'firefox'
-        ? {
-            pages: 'builds/firefox',
-            assets: 'builds/firefox',
-            fallback: null,
-            precompress: false,
-            manifest: 'manifest_firefox.json',
-          }
-        : {
-            pages: 'builds/chrome',
-            assets: 'builds/chrome',
-            fallback: null,
-            precompress: false,
-            manifest: 'manifest_chrome.json',
-          }
-    ),
+    adapter: adapter({
+      pages: `builds/${buildMode}`,
+      assets: `builds/${buildMode}`,
+      fallback: null,
+      precompress: false,
+      manifest: `manifest_${buildMode}.json`,
+    }),
     appDir: 'app',
   },
 }
