702b46398a3544754e2cfdc990b5e1d532be2dcf
Hunteraulo1 <hunteraulo@pm.me>
2024-07-05 17:13:52 +0200

    :bug: scrollbar fix
    Closes #11

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -2,6 +2,7 @@
 import { goto } from '$app/navigation'
 import { Button } from '$lib/components/ui/button/index.js'
 import Label from '$lib/components/ui/label/label.svelte'
+import { ScrollArea } from '$lib/components/ui/scroll-area'
 import Switch from '$lib/components/ui/switch/switch.svelte'
 import { settings } from '$lib/stores'
 import type { Settings } from '$lib/types'
@@ -68,53 +69,55 @@ const handleSettings = (id: keyof Settings) => {
 </script>
 
 <div class="p-2 flex flex-col gap-8" id="0">
-  <div class="flex flex-col gap-2">
-    <h1 class="text-center mb-2 font-bold">Paramètres</h1>
+  <ScrollArea class="relative">
+    <div class="flex flex-col gap-2">
+      <h1 class="text-center mb-2 font-bold">Paramètres</h1>
 
-    {#each settingsItems as { title, id }}
-      <div class="flex justify-center items-center gap-2">
-        <Label for={id}>{title}</Label>
+      {#each settingsItems as { title, id }}
+        <div class="flex justify-center items-center gap-2">
+          <Label for={id}>{title}</Label>
 
-        {#if id !== "theme"}
-          <Switch
-            {id}
-            on:click={() => handleSettings(id)}
-            checked={$settings[id]}
-          />
-        {:else}
-          <Button {id} on:click={toggleMode} variant="outline" size="icon">
-            <Sun
-              class="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          {#if id !== "theme"}
+            <Switch
+              {id}
+              on:click={() => handleSettings(id)}
+              checked={$settings[id]}
             />
-            <Moon
-              class="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-            />
-            <span class="sr-only">Changer le thème</span>
-          </Button>
-        {/if}
-      </div>
-    {/each}
-  </div>
-
-  <div>
-    <h2 class="text-center mb-2 font-bold">En savoir plus</h2>
-    <div class="flex justify-center items-center flex-col">
-      <Button variant="outline" on:click={() => goto("/traductors#0")}
-        >Voir les traducteurs/relecteurs</Button
-      >
-
-      {#each links as { title, href }}
-        <a {href} target="_blank">
-          {#if title !== "Accèder au Discord"}
-            <Button variant="link" class="flex gap-1">{title}</Button>
           {:else}
-            <Button variant="link" class="flex gap-1">
-              <DiscordLogo class="h-6 w-6" />
-              Accèder au Discord
+            <Button {id} on:click={toggleMode} variant="outline" size="icon">
+              <Sun
+                class="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+              />
+              <Moon
+                class="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+              />
+              <span class="sr-only">Changer le thème</span>
             </Button>
           {/if}
-        </a>
+        </div>
       {/each}
     </div>
-  </div>
+
+    <div>
+      <h2 class="text-center mb-2 font-bold">En savoir plus</h2>
+      <div class="flex justify-center items-center flex-col">
+        <Button variant="outline" on:click={() => goto("/traductors#0")}
+          >Voir les traducteurs/relecteurs</Button
+        >
+
+        {#each links as { title, href }}
+          <a {href} target="_blank">
+            {#if title !== "Accèder au Discord"}
+              <Button variant="link" class="flex gap-1">{title}</Button>
+            {:else}
+              <Button variant="link" class="flex gap-1">
+                <DiscordLogo class="h-6 w-6" />
+                Accèder au Discord
+              </Button>
+            {/if}
+          </a>
+        {/each}
+      </div>
+    </div>
+  </ScrollArea>
 </div>
--- src/routes/traductors/+page.svelte
+++ src/routes/traductors/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { Button } from '$lib/components/ui/button'
 import * as Card from '$lib/components/ui/card'
+import { ScrollArea } from '$lib/components/ui/scroll-area'
 import { type TraductorType, TraductorsData } from '$lib/schemas'
 import { onMount } from 'svelte'
 import { Reload } from 'svelte-radix'
@@ -43,36 +44,38 @@ const query = async (): Promise<TraductorType[]> => {
 
 {#await queryResult then traductors}
   {#if traductors.length > 0}
-    <div class="flex flex-col gap-4 max-h-full p-2 relative" id="0">
-      {#each traductors as { name, pages, tradCount, readCount }}
-        <Card.Root class="relative">
-          <Card.CardContent class="p-2 min-h-20">
-            <p class="font-bold text-center">{name}</p>
+    <ScrollArea class="relative">
+      <div class="flex flex-col gap-4 max-h-full p-2 relative" id="0">
+        {#each traductors as { name, pages, tradCount, readCount }}
+          <Card.Root class="relative">
+            <Card.CardContent class="p-2 min-h-20">
+              <p class="font-bold text-center">{name}</p>
 
-            <div class="flex text-sm items-center justify-center">
-              <p class="font-bold text-secondary-foreground/50">Liens:</p>
-              {#each pages as { title, link }}
-                <a href={link} target="_blank">
-                  <Button variant="link" class="px-1">
-                    {title}
-                  </Button>
-                </a>
-              {:else}
-                <p class="px-1 py-2">Aucun lien</p>
-              {/each}
-            </div>
-            <div class="flex justify-around w-full">
-              <p class="text-sm font-bold text-secondary-foreground/50">
-                Traduction: <span class="text-secondary-foreground">{tradCount}</span>
-              </p>
-              <p class="text-sm font-bold text-secondary-foreground/50">
-                Relecture: <span class="text-secondary-foreground">{readCount}</span>
-              </p>
-            </div>
-          </Card.CardContent>
-        </Card.Root>
-      {/each}
-    </div>
+              <div class="flex text-sm items-center justify-center">
+                <p class="font-bold text-secondary-foreground/50">Liens:</p>
+                {#each pages as { title, link }}
+                  <a href={link} target="_blank">
+                    <Button variant="link" class="px-1">
+                      {title}
+                    </Button>
+                  </a>
+                {:else}
+                  <p class="px-1 py-2">Aucun lien</p>
+                {/each}
+              </div>
+              <div class="flex justify-around w-full">
+                <p class="text-sm font-bold text-secondary-foreground/50">
+                  Traduction: <span class="text-secondary-foreground">{tradCount}</span>
+                </p>
+                <p class="text-sm font-bold text-secondary-foreground/50">
+                  Relecture: <span class="text-secondary-foreground">{readCount}</span>
+                </p>
+              </div>
+            </Card.CardContent>
+          </Card.Root>
+        {/each}
+      </div>
+    </ScrollArea>
   {:else}
     <div class="flex justify-center items-center min-h-full">
       <Button>
