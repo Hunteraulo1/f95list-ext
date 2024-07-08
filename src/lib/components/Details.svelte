0bbb63d2f6563931673e2b3d96b4b83a50b28031
Hunteraulo1 <hunteraulo@pm.me>
2024-07-08 18:52:18 +0200

    :dizzy: fix animation details

--- src/lib/components/Details.svelte
+++ src/lib/components/Details.svelte
@@ -15,86 +15,87 @@ export let game: GameType, open: boolean
 let closed = false
 </script>
 
-<ScrollArea class="fixed top-0 left-0 w-full h-full max-w-[300px] max-h-[448px] z-20 bg-primary-foreground {closed ? 'animate-toUp' : 'animate-toDown'}">
-  {#if game}
-    <img src={game.image ?? noImage} alt={game.name} class="h-32 w-full object-cover" />
-
-    <div class="p-2 flex flex-col gap-2">
-      <h2>
-        <span class="font-bold text-sm">Site:</span>
-        <a href={game.link} target="_blank">
-          <Badge variant="secondary">{game.domain}</Badge>
-        </a>
-      </h2>
-      <h1>
-        <Badge style={typeColor(game.type)} class="text-white font-bold">{game.type}</Badge>
-        <Badge style={statusColor(game.status)} class="text-white font-bold">{game.status}</Badge>
-        {game.name}
-        <span class="text-xs font-bold {game.tversion === game.version ? 'text-green-700' : 'text-red-700'}">
-          <Tooltip.Root>
-            <Tooltip.Trigger>{game.tversion}</Tooltip.Trigger>
-            <Tooltip.Content class="bg-transparent">
-              <Badge variant="secondary">
-                {#if game.tversion === game.version}
-                  À jour
-                {:else if game.tversion === 'n/a'}
-                  Pas de traduction
-                {:else}
-                  N'est pas à jour ({game.version})
-                {/if}
-              </Badge>
-            </Tooltip.Content>
-          </Tooltip.Root>
-        </span>
-      </h1>
-      <div class="flex gap-1 flex-wrap">
-        <span class="font-bold text-sm">Tags:</span>
-        {#each game.tags as tag, index}
-          {#if index < 5 || !tagsHide}
-            <Badge variant="outline">{tag}</Badge>
-          {/if}
-        {/each}
-        {#if game.tags.length > 5}
-          <button class="text-xs font-bold text-secondary-foreground/50" on:click={() => (tagsHide = !tagsHide)}>
-            {tagsHide ? 'afficher plus...' : 'cacher'}
-          </button>
-        {/if}
-      </div>
-      <p class="text-sm">
-        <span class="font-bold">Traducteur:</span>
-        <a href={game.trlink} class:traductor={game.trlink} target="_blank">
-          {game.traductor ?? 'Aucun'}
-        </a>
-        <span class="text-secondary-foreground/50 text-xs">({game.ttype})</span>
-      </p>
-      {#if game.proofreader}
-        <p class="text-sm">
-          <span class="font-bold">Relecteur:</span>
-          {game.proofreader}
-        </p>
-      {/if}
-      <div class="flex justify-center mt-2">
-        {#if game.tname === 'Pas de traduction'}
-          <Button variant="ghost" class="flex gap-1">Aucune traduction</Button>
-        {:else if game.tname === 'Intégrée'}
-          <Button variant="outline" class="flex gap-1">Traduction intégrée</Button>
-        {:else}
-          <a href={game.tlink} target="_blank">
-            <Button class="flex gap-1">Accèder à la traduction</Button>
+<div class="fixed top-0 left-0 w-full h-full max-w-[300px] max-h-[448px] z-20">
+  <ScrollArea class="bg-primary-foreground h-full {closed ? 'animate-toUp' : 'animate-toDown'}">
+    <Button class="flex gap-1 opacity-50 absolute top-2 left-2" variant="secondary" on:click={() => {
+      closed = true
+      setTimeout(() => open = false, 600)
+    }}>
+      <ArrowLeft />
+    </Button>
+    {#if game}
+      <img src={game.image ?? noImage} alt={game.name} class="h-32 w-full object-cover" />
+      <div class="p-2 flex flex-col gap-2">
+        <h2>
+          <span class="font-bold text-sm">Site:</span>
+          <a href={game.link} target="_blank">
+            <Badge variant="secondary">{game.domain}</Badge>
           </a>
+        </h2>
+        <h1>
+          <Badge style={typeColor(game.type)} class="text-white font-bold">{game.type}</Badge>
+          <Badge style={statusColor(game.status)} class="text-white font-bold">{game.status}</Badge>
+          {game.name}
+          <span class="text-xs font-bold {game.tversion === game.version ? 'text-green-700' : 'text-red-700'}">
+            <Tooltip.Root>
+              <Tooltip.Trigger>{game.tversion}</Tooltip.Trigger>
+              <Tooltip.Content class="bg-transparent">
+                <Badge variant="secondary">
+                  {#if game.tversion === game.version}
+                    À jour
+                  {:else if game.tversion === 'n/a'}
+                    Pas de traduction
+                  {:else}
+                    N'est pas à jour ({game.version})
+                  {/if}
+                </Badge>
+              </Tooltip.Content>
+            </Tooltip.Root>
+          </span>
+        </h1>
+        <div class="flex gap-1 flex-wrap">
+          <span class="font-bold text-sm">Tags:</span>
+          {#each game.tags as tag, index}
+            {#if index < 5 || !tagsHide}
+              <Badge variant="outline">{tag}</Badge>
+            {/if}
+          {/each}
+          {#if game.tags.length > 5}
+            <button class="text-xs font-bold text-secondary-foreground/50" on:click={() => (tagsHide = !tagsHide)}>
+              {tagsHide ? 'afficher plus...' : 'cacher'}
+            </button>
+          {/if}
+        </div>
+        <p class="text-sm">
+          <span class="font-bold">Traducteur:</span>
+          <a href={game.trlink} class:traductor={game.trlink} target="_blank">
+            {game.traductor ?? 'Aucun'}
+          </a>
+          <span class="text-secondary-foreground/50 text-xs">({game.ttype})</span>
+        </p>
+        {#if game.proofreader}
+          <p class="text-sm">
+            <span class="font-bold">Relecteur:</span>
+            {game.proofreader}
+          </p>
         {/if}
+        <div class="flex justify-center mt-2">
+          {#if game.tname === 'Pas de traduction'}
+            <Button variant="ghost" class="flex gap-1">Aucune traduction</Button>
+          {:else if game.tname === 'Intégrée'}
+            <Button variant="outline" class="flex gap-1">Traduction intégrée</Button>
+          {:else}
+            <a href={game.tlink} target="_blank">
+              <Button class="flex gap-1">Accèder à la traduction</Button>
+            </a>
+          {/if}
+        </div>
       </div>
-    </div>
-  {:else}
-    <h1>Game not found</h1>
-  {/if}
-  <Button class="flex gap-1 opacity-50 absolute top-2 left-2" variant="secondary" on:click={() => {
-    closed = true
-    setTimeout(() => open = false, 600)
-  }}>
-    <ArrowLeft />
-  </Button>
-</ScrollArea>
+    {:else}
+      <h1>Game not found</h1>
+    {/if}
+  </ScrollArea>
+</div>
 
 <style lang="postcss">
   .traductor {
