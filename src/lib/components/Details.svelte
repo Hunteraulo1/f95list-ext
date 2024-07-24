3b877322976b62e7b99f8393f551fb73343b878a
Hunteraulo1 <hunteraulo@pm.me>
2024-07-25 00:05:57 +0200

    :children_crossing: change link text

--- src/lib/components/Details.svelte
+++ src/lib/components/Details.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-import noImage from '$lib/assets/no-image.png'
-import * as Alert from '$lib/components/ui/alert/index.js'
-import { Badge } from '$lib/components/ui/badge'
-import { Button } from '$lib/components/ui/button/index.js'
-import { ScrollArea } from '$lib/components/ui/scroll-area/index.js'
-import * as Tooltip from '$lib/components/ui/tooltip'
-import type { GameType } from '$lib/schemas'
-import { settings } from '$lib/stores'
-import { statusColor, typeColor } from '$lib/utils/badgeColor'
-import { lazyLoad } from '$lib/utils/lazyload'
-import { ArrowLeft, ExclamationTriangle } from 'svelte-radix'
+  import noImage from '$lib/assets/no-image.png'
+  import * as Alert from '$lib/components/ui/alert/index.js'
+  import { Badge } from '$lib/components/ui/badge'
+  import { Button } from '$lib/components/ui/button/index.js'
+  import { ScrollArea } from '$lib/components/ui/scroll-area/index.js'
+  import * as Tooltip from '$lib/components/ui/tooltip'
+  import type { GameType } from '$lib/schemas'
+  import { settings } from '$lib/stores'
+  import { statusColor, typeColor } from '$lib/utils/badgeColor'
+  import { lazyLoad } from '$lib/utils/lazyload'
+  import { ArrowLeft, ExclamationTriangle } from 'svelte-radix'
 
-let tagsHide = $settings.tagsHide
+  let tagsHide = $settings.tagsHide
 
-export let game: GameType, open: boolean
-let closed = false
+  export let game: GameType, open: boolean
+  let closed = false
 </script>
 
 <div class="fixed top-0 left-0 w-full h-full max-w-[300px] max-h-[448px] z-20">
@@ -44,20 +44,16 @@ let closed = false
           <span class="font-bold text-sm">Site:</span>
           <a href={game.link} target="_blank">
             <Badge variant="secondary">{game.domain}</Badge>
-            <Button variant="link"
-              >(Accèder {game.hostname
-                ? `à ${game.hostname}`
-                : 'au site'})</Button
-            >
+            <Button variant="link">(Accèder au jeu)</Button>
           </a>
         </h2>
         <h1>
-          <Badge style={typeColor(game.type)} class="text-white font-bold"
-            >{game.type}</Badge
-          >
-          <Badge style={statusColor(game.status)} class="text-white font-bold"
-            >{game.status}</Badge
-          >
+          <Badge style={typeColor(game.type)} class="text-white font-bold">
+            {game.type}
+          </Badge>
+          <Badge style={statusColor(game.status)} class="text-white font-bold">
+            {game.status}
+          </Badge>
           {game.name}
           <span
             class="text-xs font-bold {game.tversion === game.version
@@ -101,9 +97,9 @@ let closed = false
           <a href={game.trlink} class:traductor={game.trlink} target="_blank">
             {game.traductor ?? 'Aucun'}
           </a>
-          <span class="text-secondary-foreground/50 text-xs"
-            >({game.ttype})</span
-          >
+          <span class="text-secondary-foreground/50 text-xs">
+            ({game.ttype})
+          </span>
         </p>
         {#if game.proofreader}
           <p class="text-sm">
@@ -137,9 +133,9 @@ let closed = false
             <Button variant="ghost" class="flex gap-1">Aucune traduction</Button
             >
           {:else if game.tname === 'Intégrée'}
-            <Button variant="outline" class="flex gap-1"
-              >Traduction intégrée</Button
-            >
+            <Button variant="outline" class="flex gap-1">
+              Traduction intégrée
+            </Button>
           {:else}
             <a href={game.tlink} target="_blank">
               <Button class="flex gap-1">Accéder à la traduction</Button>
