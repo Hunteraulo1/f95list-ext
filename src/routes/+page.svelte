d5c2153ddd8b619123f57d54783dcfe1bd29f399
Hunteraulo1 <hunteraulo@pm.me>
2024-07-24 13:42:46 +0200

    :bug: fix idGameBoxPromise with devMode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,56 @@
 <script lang="ts">
-import Filter from '$lib/components/Filter.svelte'
-import GameBox from '$lib/components/GameBox.svelte'
-import { ScrollArea } from '$lib/components/ui/scroll-area'
-import { filteredGames, settings } from '$lib/stores'
-import type { IdGameBox } from '$lib/types'
-import Reload from 'svelte-radix/Reload.svelte'
-import type { Tabs } from 'webextension-polyfill'
-import Button from './../lib/components/ui/button/button.svelte'
+  import { dev } from '$app/environment'
+  import Filter from '$lib/components/Filter.svelte'
+  import GameBox from '$lib/components/GameBox.svelte'
+  import { ScrollArea } from '$lib/components/ui/scroll-area'
+  import { filteredGames, settings } from '$lib/stores'
+  import type { IdGameBox } from '$lib/types'
+  import Reload from 'svelte-radix/Reload.svelte'
+  import type { Tabs } from 'webextension-polyfill'
+  import Button from './../lib/components/ui/button/button.svelte'
 
-let browserAPI = undefined
+  let browserAPI = undefined
 
-if (typeof chrome !== 'undefined') {
-  browserAPI = chrome
-} else if (typeof browser !== 'undefined') {
-  browserAPI = browser
-}
+  if (typeof chrome !== 'undefined') {
+    browserAPI = chrome
+  } else if (typeof browser !== 'undefined') {
+    browserAPI = browser
+  }
 
-const extractId = (inputString: string): number => {
-  const regex = /\.(\d+)/
-  const match = inputString.match(regex)
+  const extractId = (inputString: string): number => {
+    const regex = /\.(\d+)/
+    const match = inputString.match(regex)
 
-  return match ? parseInt(match[1]) : 0
-}
+    return match ? parseInt(match[1]) : 0
+  }
 
-const idGameBoxPromise: Promise<IdGameBox> = new Promise(resolve =>
-  browserAPI?.tabs?.query({ active: true, currentWindow: true }, (tabs: Tabs.Tab[]) => {
-    const { url } = tabs[0]
-    console.log('🚀 ~ browserAPI?.tabs?.query ~ url:', url)
+  const idGameBoxPromise: Promise<IdGameBox> = new Promise(resolve =>
+    dev
+      ? resolve({ domain: 'Unknown', id: 0 })
+      : browserAPI?.tabs?.query(
+          { active: true, currentWindow: true },
+          (tabs: Tabs.Tab[]) => {
+            const { url } = tabs[0]
+            console.log('🚀 ~ browserAPI?.tabs?.query ~ url:', url)
 
-    if ($settings.autoFocusGame && url?.startsWith('https://f95zone.to/threads/')) {
-      resolve({ domain: 'F95z', id: extractId(url) })
-    }
+            if (
+              $settings.autoFocusGame &&
+              url?.startsWith('https://f95zone.to/threads/')
+            ) {
+              resolve({ domain: 'F95z', id: extractId(url) })
+            }
 
-    if ($settings.autoFocusGame && url?.startsWith('https://lewdcorner.com/threads/')) {
-      resolve({ domain: 'LewdCorner', id: extractId(url) })
-    }
+            if (
+              $settings.autoFocusGame &&
+              url?.startsWith('https://lewdcorner.com/threads/')
+            ) {
+              resolve({ domain: 'LewdCorner', id: extractId(url) })
+            }
 
-    resolve({ domain: 'Unknown', id: 0 })
-  })
-)
+            resolve({ domain: 'Unknown', id: 0 })
+          }
+        )
+  )
 </script>
 
 <ScrollArea class="relative min-h-[448px]">
