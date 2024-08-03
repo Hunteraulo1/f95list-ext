a3eff781ab3ccefcf8b610d4442b6180e053fbe5
Hunteraulo1 <hunteraulo@pm.me>
2024-08-03 11:34:53 +0200

    :children_crossing: revert page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,21 +5,6 @@ import getData from '$lib/utils/getData'
 import { ModeWatcher } from 'mode-watcher'
 import { Reload } from 'svelte-radix'
 import '../app.postcss'
-
-import { onNavigate } from '$app/navigation'
-
-onNavigate(navigation => {
-  // @ts-ignore
-  if (!document.startViewTransition) return
-
-  return new Promise(resolve => {
-    // @ts-ignore
-    document.startViewTransition(async () => {
-      resolve()
-      await navigation.complete
-    })
-  })
-})
 </script>
 
 <ModeWatcher defaultMode="system" />
