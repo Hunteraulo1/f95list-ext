1f4279833b730ecec765af55c2efbcc27d29d7ae
Hunteraulo1 <hunteraulo@pm.me>
2024-09-07 08:47:03 +0200

    :bug: fix popupDetail on webapp

--- src/routes/webapp/+page.svelte
+++ src/routes/webapp/+page.svelte
@@ -3,17 +3,14 @@ import GameBox from '$lib/components/GameBox.svelte';
 import Button from '$lib/components/ui/button/button.svelte';
 import { ScrollArea } from '$lib/components/ui/scroll-area';
 import { detailGame, filteredGames } from '$lib/stores';
-import type { IdGameBox } from '$lib/types';
 import Reload from 'svelte-radix/Reload.svelte';
-
-const idGameBox: IdGameBox = { domain: 'Unknown', id: 0 };
 </script>
 
 <ScrollArea class="relative h-full">
   <div class="flex flex-col gap-2 p-2 relative h-full">
     {#each $filteredGames as game (game.name + game.version)}
       <button on:click={() => $detailGame = game}>
-        <GameBox {game} {idGameBox} webapp />
+        <GameBox {game} webapp />
       </button>
     {:else}
       <div class="flex justify-center items-center h-full">
