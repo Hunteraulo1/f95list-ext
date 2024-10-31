a3bb2da52e3c850b733931d26462ce3e6edc11f0
Hunteraulo1 <hunteraulo@pm.me>
2024-10-31 10:07:39 +0100

    :bug: fix filter

--- src/lib/components/FilterPopover.svelte
+++ src/lib/components/FilterPopover.svelte
@@ -15,60 +15,62 @@ type Props = {
 let { title, values }: Props = $props();
 </script>
 
-<label
-  for={title}
-  class="font-bold text-xs capitalize leading-none mt-2"
-  >{title}:
-</label>
-<Popover.Root>
-  <Popover.Trigger class={buttonVariants({ variant: "outline", className: "w-full flex justify-between" })}>
-    {#if values.some(({ checked }) => checked)}
-      {values.reduce((acc, { checked, value }) => 
-        checked ? acc ? `${acc}, ${value}` : value : acc
-      , "")}
-    {:else}
-      Filtrer par {title}...
-    {/if}
-    <ChevronDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
-  </Popover.Trigger>
-  <Popover.Content class="w-fit p-0">
-    <Command.Root>
-      <Command.Input placeholder="Rechercher..." />
-      <Command.Empty>Aucun {title} trouvé</Command.Empty>
-      <Command.Group class="max-h-full relative">
-        <ScrollArea class="h-[16rem]">
-          {#each values as { value, checked }}
-            <Command.Item
-              {value}
-              onSelect={() => {
-                filter.update((items) => {
-                  return items.map(item => {
-                    if (item.title === title) {
-                      return {
-                        ...item,
-                        values: item.values.map(v => {
-                          if (v.value === value) {
-                            return { ...v, checked: !v.checked };
-                          }
-
-                          return v;
-                        })
-                      };
-                    }
-
-                    return item;
+<div class="w-full">
+  <label
+    for={title}
+    class="font-bold text-xs capitalize leading-none mt-2"
+    >{title}:
+  </label>
+  <Popover.Root>
+    <Popover.Trigger class={buttonVariants({ variant: "outline", class: "w-full flex justify-between" })}>
+      {#if values.some(({ checked }) => checked)}
+        {values.reduce((acc, { checked, value }) =>
+          checked ? acc ? `${acc}, ${value}` : value : acc
+        , "")}
+      {:else}
+        Filtrer par {title.length > 17 ? title.slice(0, 17) : title}...
+      {/if}
+      <ChevronDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+    </Popover.Trigger>
+    <Popover.Content class="w-fit p-0">
+      <Command.Root>
+        <Command.Input placeholder="Rechercher..." />
+        <Command.Empty>Aucun {title} trouvé</Command.Empty>
+        <Command.Group class="max-h-full relative">
+          <ScrollArea class="h-[16rem]">
+            {#each values as { value, checked }}
+              <Command.Item
+                {value}
+                onSelect={() => {
+                  filter.update((items) => {
+                    return items.map(item => {
+                      if (item.title === title) {
+                        return {
+                          ...item,
+                          values: item.values.map(v => {
+                            if (v.value === value) {
+                              return { ...v, checked: !v.checked };
+                            }
+  
+                            return v;
+                          })
+                        };
+                      }
+  
+                      return item;
+                    });
                   });
-                });
-              }}
-            >
-              <Check class={cn("mr-2 h-4 w-4", {
-                "text-transparent": !checked
-              })} />
-              {value}
-            </Command.Item>
-          {/each}
-        </ScrollArea>
-      </Command.Group>
-    </Command.Root>
-  </Popover.Content>
-</Popover.Root>
+                }}
+              >
+                <Check class={cn("mr-2 h-4 w-4", {
+                  "text-transparent": !checked
+                })} />
+                {value}
+              </Command.Item>
+            {/each}
+          </ScrollArea>
+        </Command.Group>
+      </Command.Root>
+    </Popover.Content>
+  </Popover.Root>
+</div>
