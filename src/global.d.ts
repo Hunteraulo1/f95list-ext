909108fdc268c74f700613aeac6cd2b06e210802
Hunteraulo1 <hunteraulo@pm.me>
2024-11-05 10:51:21 +0100

    :label: upgrade lazy type

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,15 @@
 declare module 'svelte-lazy' {
-  import { SvelteComponentTyped as SvelteComponent } from 'svelte';
+  import { SvelteComponent } from 'svelte';
+  import type { FadeParams } from 'svelte/transition';
+
   export default class Lazy extends SvelteComponent<{
-    height?: number;
+    height?: number | string;
+    keep?: boolean;
+    offset?: number;
+    placeholder?: string | SvelteComponent;
+    class?: string;
+    fadeOption?: FadeParams;
+    onload?: (node: HTMLElement) => void;
+    resetHeightDelay?: number;
   }> {}
 }
