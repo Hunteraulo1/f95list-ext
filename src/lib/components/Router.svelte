537562321be5900d44ee87ccf3d8dbf9e6d3081b
Hunteraulo1 <hunteraulo@pm.me>
2025-01-11 13:37:08 +0100

    :tada: init new build project

---
+++ src/lib/components/Router.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+import type { Component } from 'svelte';
+import { page } from '../stores';
+
+interface Props {
+  pages: {
+    link: string;
+    target: Component;
+  }[];
+}
+
+const { pages }: Props = $props();
+</script>
+
+{#each pages as { link, target }}
+  {#if link === $page}
+    {target}
+  {/if}
+{/each}
