032f70e1355b0fe3034ce0b0a22cd0888bd6510c
Hunteraulo1 <hunteraulo@pm.me>
2025-01-11 13:57:55 +0100

    :sparkles: implement router

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+import type { Component } from 'svelte';
+
+export interface Page {
+  link: string;
+  target: Component;
+  icon: Component | null;
+}
+
+interface Props {
+  pages: Page[];
+}
+
+const { pages }: Props = $props();
+</script>
+
+{#each pages as {link, target, icon}}
+  <h1>Nav</h1>
+{/each}
---
+++ src/pages/Popup.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import List from '$components/List.svelte';
+import Nav, { type Page } from '$components/Nav.svelte';
+import Router from '$components/Router.svelte';
+
+const pages: Page[] = [
+  {
+    link: 'list',
+    target: List,
+    icon: null,
+  },
+];
+</script>
+
+<Router {pages} />
+
+<Nav {pages} />
