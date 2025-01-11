8ebabfb3475b1e419211f28a43e99e8f17f04f28
Hunteraulo1 <hunteraulo@pm.me>
2025-01-11 16:47:10 +0100

    :wrench: fix and config vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,36 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import tailwindcss from '@tailwindcss/vite';
 import path from 'node:path';
 import { defineConfig } from 'vite';
 import webExtension, { readJsonFile } from 'vite-plugin-web-extension';
 
-function generateManifest() {
+const generateManifest = () => {
   const manifest = readJsonFile('src/manifest.json');
   const pkg = readJsonFile('package.json');
+
   return {
-    name: pkg.name,
+    author: pkg.author.name,
     description: pkg.description,
     version: pkg.version,
     ...manifest,
   };
-}
+};
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
     webExtension({
       manifest: generateManifest,
       watchFilePaths: ['package.json', 'manifest.json'],
+      browser: process.env.TARGET || 'chrome',
+      disableAutoLaunch: true,
     }),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
-      $ui: path.resolve(__dirname, './src/lib/components/ui'),
-      $components: path.resolve(__dirname, './src/lib/components'),
-      $lib: path.resolve(__dirname, './src/lib'),
+      $ui: path.resolve('./src/lib/components/ui'),
+      $components: path.resolve('./src/lib/components'),
+      $utils: path.resolve('./src/lib/utils'),
+      $lib: path.resolve('./src/lib'),
     },
   },
 });
