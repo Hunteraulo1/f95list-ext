72214e5212ee799fccef208a2811a25ca9f6c5bb
Hunteraulo1 <hunteraulo@pm.me>
2025-01-11 16:48:09 +0100

    :construction: start new design

---
+++ src/lib/components/ui/Button.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { cn } from '$lib/utils/cn.js';
+import type { Component } from 'svelte';
+import type { MouseEventHandler } from 'svelte/elements';
+import type { ClassNameValue } from 'tailwind-merge';
+
+interface Props {
+  icon?: Component
+  title?: string
+  classIcon?: ClassNameValue
+  className?: ClassNameValue
+  onclick?: MouseEventHandler<HTMLButtonElement>
+}
+
+let { icon, title, className, classIcon, onclick}: Props = $props();
+</script>
+
+<button class={className?.toString()} {onclick}>
+  {#if icon}
+    {@const Icon = icon}
+    <Icon class={cn("mr-2 h-4 w-4 animate-spin", classIcon)} />
+  {/if}
+
+  {title}
+</button>
