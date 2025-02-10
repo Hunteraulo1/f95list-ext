475f8b359a2e3cc56b2ab941f77d3320fc96a3f3
Hunteraulo1 <hunteraulo@pm.me>
2025-02-10 18:43:32 +0100

    :rotating_light: biome lint

--- src/popup.ts
+++
@@ -1,5 +0,0 @@
-import { mount } from 'svelte';
-import Popup from './pages/Popup.svelte';
-import Webapp from './pages/Webapp.svelte';
-
-mount(window.location.hash === '#webapp' ? Webapp : Popup, { target: document.body });
