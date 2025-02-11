9657a17e2bb022f1f09567aaf560c7c50a9fc8c8
Hunteraulo1 <hunteraulo@pm.me>
2025-02-11 11:38:01 +0100

    :lipstick: improve nav

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -28,22 +28,22 @@ const handleClick = (link: Page['link'], target: Page['target']) => {
 };
 </script>
 
-<nav class={cn("flex w-full gap-2 justify-around bg-primary-foreground p-1 border-t-4 border-secondary/60", variant === 'webapp' && 'rounded-md')}>
+<nav class={cn("flex w-full gap-2 justify-around bg-primary-foreground p-1 border-t-4 border-secondary/60 h-14", variant === 'webapp' && 'rounded-md h-18')}>
   {#each pages as {icon, name, link, className, target}, index}
     {#if icon}
       {@const Icon = icon}
       <button
-        class="flex p-1 flex-1 flex-col justify-center items-center hover:bg-secondary/60 rounded-md transition-all duration-300"
+        class={cn("flex flex-1 flex-col justify-center items-center hover:bg-secondary/60 rounded-md transition-all duration-300 hover:text-white text-white/50", $page === link && 'text-white')}
         onmouseenter={() => mouseEnter[index] = true}
         onmouseleave={() => mouseEnter[index] = false}
         onclick={() => handleClick(link, target)}
       >
-      <Icon class={cn("mr-2 h-4 w-4", className)} isHovered={mouseEnter[index]} />
-      
-      <span class={cn("text-xs opacity-70", mouseEnter[index] ? 'animate-pulse' : '')}>
-        {name}
-      </span>
-    </button>
+        <Icon class={cn("mr-2 transition-all", className)} isHovered={mouseEnter[index]} size={mouseEnter[index] ? "1.5rem" : "1.7rem"} />
+        
+        <span class={cn("text-[.6rem] text-white/0 leading-0 transition-all", mouseEnter[index] && 'text-white animate-pulse leading-3')}>
+          {name}
+        </span>
+      </button>
     {/if}
   {/each}
 </nav>
