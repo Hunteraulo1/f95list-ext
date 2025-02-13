d5e208b387cdc930e4899345569a827196184bc3
Hunteraulo1 <hunteraulo@pm.me>
2025-02-13 13:41:44 +0100

    :children_crossing: cursor pointer for 'more tags...'

--- src/lib/components/Details.svelte
+++ src/lib/components/Details.svelte
@@ -106,7 +106,7 @@ let closeHovered = $state<boolean>(false);
           {/each}
           {#if game.tags.length > 5}
             <button
-              class="text-xs font-bold text-secondary-foreground/50"
+              class="text-xs font-bold text-secondary-foreground/50 cursor-pointer"
               onclick={() => (tagsHide = !tagsHide)}
             >
               {tagsHide ? 'afficher plus...' : 'cacher'}
