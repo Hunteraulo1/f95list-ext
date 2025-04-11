e015aebb0423b9a17e5c8c5ee7dbf354920284c8
Hunteraulo1 <hunteraulo@pm.me>
2025-04-11 15:03:11 +0200

    :lipstick: implement not animate moon

--- src/lib/assets/icon/index.ts
+++ src/lib/assets/icon/index.ts
@@ -1,4 +1,3 @@
-import { Moon } from 'lucide-svelte';
 import ArrowLeft from './ArrowLeft.svelte';
 import BadgeAlert from './BadgeAlert.svelte';
 import Bell from './Bell.svelte';
@@ -6,6 +5,7 @@ import Check from './Check.svelte';
 import ChevronDown from './ChevronDown.svelte';
 import CopyCheck from './CopyCheck.svelte';
 import Expand from './Expand.svelte';
+import Moon from './Moon.svelte';
 import Plus from './Plus.svelte';
 import RefreshCcw from './RefreshCcw.svelte';
 import ScanText from './ScanText.svelte';
