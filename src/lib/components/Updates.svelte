637ebe2be60a84eb65eb30cee5130eea9a195c19
Hunteraulo1 <hunteraulo@pm.me>
2025-05-08 10:12:01 +0200

    :construction: WIP updates filter feature

--- src/lib/components/Updates.svelte
+++ src/lib/components/Updates.svelte
@@ -4,6 +4,7 @@ import GameBox from '@/lib/components/GameBox.svelte';
 import { Button } from '@/lib/components/ui/button';
 import { ScrollArea } from '@/lib/components/ui/scroll-area';
 import { updates } from '@/lib/stores';
+import type { UpdateType } from '../schemas';
 
 browser.runtime.sendMessage('f95list-badge');
 
@@ -11,13 +12,25 @@ interface Props {
   webapp?: boolean;
 }
 
+let filterType = $state<UpdateType['type'] | null>(null);
+
 const { webapp = false }: Props = $props();
+
+const handleClickFilter = (type: UpdateType['type']) => {
+  if (type === filterType) {
+    filterType = null;
+
+    return;
+  }
+
+  filterType = type;
+};
 </script>
 
 {#if $updates}
   <ScrollArea class="pb-2 h-full w-full">
     <div class="flex flex-col gap-4 h-full p-2">
-      {#each $updates as update, index}
+      {#each filterType ? $updates.filter((update)=>update.type === filterType) : $updates as update, index}
         <div class="flex flex-col gap-2">
           {#if index === 0 || $updates[index - 1].date.getTime() !== update.date.getTime()}
             <h2 class="text-center text-base font-bold leading-none mt-2">
@@ -36,6 +49,10 @@ const { webapp = false }: Props = $props();
           {/each}
         </div>
       {/each}
+    </div>
+    <div class="flex justify-center gap-2 w-full fixed bottom-16">
+      <Button variant='outline' onclick={()=>handleClickFilter('AJOUT DE JEU')}>add</Button>
+      <Button variant='outline' onclick={()=>handleClickFilter('MISE À JOUR')}>edit</Button>
     </div>
   </ScrollArea>
 {:else}
